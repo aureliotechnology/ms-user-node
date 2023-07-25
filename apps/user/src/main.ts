64aaedce8d577863a9e6425e2b9ab2c18bc01886
Aurélio Moreira <aureliomoreirared@gmail.com>
2023-07-24 21:31:45 -0300

    config: Config http from Fastify

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    UserModule,
+    new FastifyAdapter()
+  );
   await app.listen(3000);
 }
 bootstrap();
